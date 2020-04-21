1c49c323618eb0c716333bb094194ba8d04bfec7
Eric Griffis <dedbox@gmail.com>
2020-04-21 15:00:30 -0700

    incrementing counter with toggle state

--- app.ml
+++ app.ml
@@ -10,21 +10,33 @@ module Model = struct
 end
 
 module State = struct
-  type t = unit
+  type t = bool ref
 end
 
 module Action = struct
-  type t = Increment [@@deriving sexp]
+  type t = Increment | Toggle [@@deriving sexp]
 end
 
 let on_startup ~schedule_action _model =
   every (Time_ns.Span.of_sec 1.) (fun () -> schedule_action Action.Increment);
-  Deferred.unit
+  Deferred.return @@ ref true
 
-let create model ~old_model:_ ~inject:_ =
+let create model ~old_model:_ ~inject =
   let%map counter = model in
-  let apply_action action _ ~schedule_action:_ =
-    match action with Action.Increment -> counter + 1
+  let apply_action action is_active ~schedule_action:_ =
+    match action with
+    | Action.Increment -> if !is_active then counter + 1 else counter
+    | Action.Toggle ->
+        is_active := not !is_active;
+        counter
+  and view =
+    let open Vdom in
+    Node.div []
+      [
+        Node.text (Int.to_string counter);
+        Node.button
+          [ Attr.on_click (fun _ -> inject Action.Toggle) ]
+          [ Node.text "toggle" ];
+      ]
   in
-  Component.create ~apply_action counter
-    (Vdom.Node.text @@ Int.to_string counter)
+  Component.create ~apply_action counter view
