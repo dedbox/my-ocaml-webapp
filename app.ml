9c72706c56255a98d5657455ee6dc7a864a3e09d
Eric Griffis <dedbox@gmail.com>
2020-04-23 14:10:34 -0700

    full-page incrementing counter with toggle, no button

--- app.ml
+++ app.ml
@@ -17,26 +17,23 @@ module Action = struct
   type t = Increment | Toggle [@@deriving sexp]
 end
 
+open Action
+
 let on_startup ~schedule_action _model =
-  every (Time_ns.Span.of_sec 1.) (fun () -> schedule_action Action.Increment);
+  every (Time_ns.Span.of_sec 1.) (fun () -> schedule_action Increment);
   Deferred.return @@ ref true
 
 let create model ~old_model:_ ~inject =
   let%map counter = model in
   let apply_action action is_active ~schedule_action:_ =
     match action with
-    | Action.Increment -> if !is_active then counter + 1 else counter
-    | Action.Toggle ->
+    | Increment -> if !is_active then counter + 1 else counter
+    | Toggle ->
         is_active := not !is_active;
         counter
   and view =
-    let open Vdom in
-    Node.div []
-      [
-        Node.text (Int.to_string counter);
-        Node.button
-          [ Attr.on_click (fun _ -> inject Action.Toggle) ]
-          [ Node.text "toggle" ];
-      ]
+    let open Vdom.Node in
+    let open Vdom.Attr in
+    div [ on_click (fun _ -> inject Toggle) ] [ text (Int.to_string counter) ]
   in
   Component.create ~apply_action counter view
